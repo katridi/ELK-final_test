1fd81745e8eae4b4dbba5dd47f7bb825c3ea9146
Aleksei Katridi <Aleksei_Katridi@epam.com>
2022-07-08 03:14:18 +0300

    added fuzzy search

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from typing import Dict
 import click
 
 from app_context import create_app_context
-from commands import match_term
+from commands import match_term, fuzzy_term
 
 
 @click.command()
@@ -14,6 +14,16 @@ from commands import match_term
 @click.option("--value", help="value for the term")
 def match(ctx, term, value):
     query: Dict = match_term(term=term, value=value)
+    res = ctx.obj.es.search(index=ctx.obj.index, body=query)
+    print(json.dumps(res, indent=4, ensure_ascii=False))
+
+
+@click.command()
+@click.pass_context
+@click.option("--term", default="customer_first_name.keyword", help="term for quering")
+@click.option("--value", help="value for the term")
+def fuzzy(ctx, term, value):
+    query: Dict = fuzzy_term(term=term, value=value)
     res = ctx.obj.es.search(index=ctx.obj.index, body=query)
     print(json.dumps(res, indent=4, ensure_ascii=False))
 
@@ -36,6 +46,7 @@ def cli(ctx):
 if __name__ == "__main__":
     cli.add_command(match)
     cli.add_command(raw)
+    cli.add_command(fuzzy)
     # make decorator to pass es
     app_context = create_app_context()
     cli(obj=app_context)
